8e7a9e38a2d0a981235d338bfa442b766365085c
Yu Heng Lee <lee2000yh@gmail.com>
2022-12-02 17:43:50 +0800

    Day2 AOC

--- Day2.py
+++ Day2.py
@@ -1,8 +1,9 @@
 from sys import stdin
 
-
 ShapePoints = {"X": 1, "Y": 2, "Z": 3} #Rock paper sciccors
 WinPoints = {"W": 6, "D": 3, "L": 0} # win draw loss
+WinCondition = {"X": [0, 2], "Y": [3, 0], "Z": [6, 1]} # Gives winPoints and how many to add to opponent shape
+Opponent = {"A": 0, "B": 1, "C": 2}
 
 def firstStar():
   points = 0
@@ -29,10 +30,21 @@ def firstStar():
     #print("a: ", a)
     #print("b: ", b)
   
+def secondStar():
+  points = 0
+  for line in stdin:
+    line.strip()
+    a, b  = line.split(" ")
+    b = b[0]
+    check = WinCondition[b]
+    points += check[0]
+    points += ((Opponent[a] + check[1]) % 3) + 1
+    
 
   print("POINTS: ", points)
 
 if __name__ == "__main__":
-  firstStar()
+  #firstStar()
+  secondStar()
 
 
